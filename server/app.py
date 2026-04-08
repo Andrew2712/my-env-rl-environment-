21e82ca7f7f14bd5dbebb8b35173852424b359ee
Andrew Clinton <andrewclinton2712@gmail.com>
2026-04-08 22:09:15 +0530

    update

---
+++ server/app.py
@@ -0,0 +1,10 @@
+from src.envs.my_env.server.app import app as env_app
+import uvicorn
+
+app = env_app
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+
+if __name__ == "__main__":
+    main()
